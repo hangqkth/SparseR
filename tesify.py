ee8d5c858f4e5db6603bd1564fb0948ceff5a890
Hang Qin <112768477+hangqkth@users.noreply.github.com>
2023-04-24 17:23:05 +0200

    Update tesify.py

--- tesify.py
+++ tesify.py
@@ -7,49 +7,67 @@ import matplotlib.pyplot as plt
 
 
 # generate matrix A with full row rank
-measurement_num = 70
-A = np.zeros((measurement_num, 100))
-for i in range(measurement_num):
-    for j in range(measurement_num):
+measure_num = 50
+sparsity = 0.30
+
+
+A = np.zeros((measure_num, 100))
+for i in range(measure_num):
+    for j in range(measure_num):
         A[i][j] = np.random.normal()
     np.random.shuffle(A[i])
 
 # specify probability distribution
-rvs = stats.norm(loc=0, scale=1).rvs
-# create sparse random matrix with specific probability distribution/random numbers.
+rvs = stats.norm(loc=0, scale=1).rvs  # standard Gaussian
+# rvs = stats.laplace().rvs  # Laplace
 
 
-# 定义优化问题
-def simulate_cvxpy(sparsity, A):
-    # generate X and Y
+def simulate_cvxpy(sparsity, A, eps=0.005):
+    """Disciplined convex programming (DCP) to solve min or max problem"""
+    # generate X from sparse function, and generate Y = A @ X
     S = sparse.random(100, 1, density=sparsity, data_rvs=rvs)
     X = S.toarray()
-    X_hat = cp.Variable((100, 1))
     Y = np.matmul(A, X)
-    objective = cp.Minimize(cp.norm(X_hat, 1))
-    constraints = [A @ X_hat == Y]
-    prob = cp.Problem(objective, constraints)
 
-    # solve the optimization problem
-    prob.solve()
+    X_hat1 = cp.Variable((100, 1))
+    X_hat2 = cp.Variable((100, 1))
 
-    # calculating ||X_hat-X||2
-    # print(X_hat.value)
-    norm = np.linalg.norm(X_hat.value - X, 2)
-    print(X.shape)
-    print(X_hat.shape)
-    x_hat = list(X_hat.value[:, ])
-    x_hat = [float(i) for i in x_hat]
+    objective1 = cp.Minimize(cp.norm(X_hat1, 1))
+    objective2 = cp.Minimize(cp.norm(X_hat2, 1))
+
+    constraints1 = [A @ X_hat1 == Y]
+    constraints2 = [cp.sum(cp.square(A @ X_hat2 - Y)) <= eps**2]
+
+    prob1 = cp.Problem(objective1, constraints1)
+    prob2 = cp.Problem(objective2, constraints2)
+
+
+    prob1.solve()
+    prob2.solve()
+
+    print(cp.norm((A @ X_hat1) - Y, 2).value)
+    print(cp.norm((A @ X_hat2) - Y, 2).value)
+
+    # calculating ||X_hat-X||
+    norm = np.linalg.norm(X_hat1.value - X, 2)
 
     plt.plot(X[:, 0])
-    plt.plot(X_hat.value)
+    plt.plot(X_hat1.value)
     plt.ylabel("value of element in x or x_hat")
     plt.xlabel("sparse vector x and x_hat")
     plt.legend(["x", "x_hat"])
+    plt.title("A@X_hat=Y, eps="+str(eps)+", sparsity="+str(sparsity)+", measurement="+str(measure_num))
     plt.show()
 
-    # print("||X_hat-X||2 = ", norm)
-    return norm, X
+    plt.plot(X[:, 0])
+    plt.plot(X_hat2.value)
+    plt.ylabel("value of element in x or x_hat")
+    plt.xlabel("sparse vector x and x_hat")
+    plt.legend(["x", "x_hat"])
+    plt.title("||A@X_hat-Y||2<=eps, eps="+str(eps)+", sparsity="+str(sparsity)+", measurement="+str(measure_num))
+    plt.show()
+
+
 
 
 def simulate_linprog(sparsity, A):
@@ -60,15 +78,19 @@ def simulate_linprog(sparsity, A):
     Y = np.matmul(A, X)
     A_eq = A
     b_eq = Y
-    c = np.ones((100, ))
+    c = np.ones((100, 1))
     bounds = [(None, None) for i in range(100)]
-    res = linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
+    print(A_eq.shape, b_eq.shape)
+    res = linprog(c, A_ub=A_eq, b_ub=b_eq, bounds=bounds, method='highs')
+
     x_hat = np.expand_dims(res.x, axis=1)
+    print(res.x)
+    print(res.status)
     error_norm = np.linalg.norm(x_hat - X, 2)
     print("||X_hat-X||2 = ", error_norm)
-    # plt.plot(list(range(100)), X)
-    # plt.plot(range(100), x_hat)
-    # plt.show()
+    plt.plot(X)
+    plt.plot(x_hat)
+    plt.show()
     return x_hat
 
 
@@ -87,7 +109,7 @@ def do_plot(s_min, s_max, A):
     # plt.show()
 
 
-e1 = simulate_cvxpy(sparsity=0.5, A=A)
-# e2, x2 = simulate_linprog(sparsity=0.05, A=A)
+simulate_cvxpy(sparsity=sparsity, A=A)
+# e2 = simulate_linprog(sparsity=0.05, A=A)
 
 
