a37d505d72f7acde97f78a7f1fd2376e0825aa46
Hang Qin <112768477+hangqkth@users.noreply.github.com>
2023-06-11 20:57:32 +0200

    add matrix completion

--- marix_completion/matrix_completion2.py
+++ marix_completion/matrix_completion2.py
@@ -1,23 +1,34 @@
 import numpy as np
 import cvxpy as cp
 
-# 定义一个稀疏矩阵，其中只有一小部分元素是已知的
-A = np.zeros((5, 5))
-A[0, 1] = 1
-A[1, 2] = 1
 
-# 定义核范数优化问题
+# A = np.zeros((5, 5))
+# A[0, 1] = 1
+# A[0, 3] = 1
+# A[1, 2] = -1
+# A[1, 3] = 1
+# A[2, 2] = -1
+# A[2, 4] = 1
+# A[3, 0] = -1
+# A[3, 3] = 1
+A = np.array([[0, 0, -1, 0, 0],
+              [0, 0, 0, 1, 0],
+              [1, 1, -1, 1, -1],
+              [1, 0, 0, 0, -1],
+              [0, 0, -1, 0, 0]])
+
+# minimization of nuclear norm to minimize rank
 X = cp.Variable((5, 5))
 objective = cp.Minimize(cp.norm(X, "nuc"))
-constraints = [X[i, j] == A[i, j] for i in range(5) for j in range(5) if A[i, j] != 0]
+constraints = [X[i, j] == A[i, j] for i in range(5) for j in range(5) if A[i, j] != 0]  # unknown entries set to 0
 problem = cp.Problem(objective, constraints)
 
-# 解决问题并输出结果
+
 result = problem.solve()
 
 if problem.status == 'optimal':
     print("Optimal value: ", result)
-    print("Recovered matrix:\n", X.value)
+    print("Recovered matrix:\n", np.round(X.value))
 else:
     print("Problem status: ", problem.status)
 
